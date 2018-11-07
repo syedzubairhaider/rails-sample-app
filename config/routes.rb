44582b60947266d02e0fc095dafe8653754eb08b
Zubair <zubair.haider31@gmail.com>
2018-11-07 11:21:36 +0500

    rename app name, linking contact page and adding test for contact

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get  '/help', to: 'static_pages#help'
   get  '/about', to: 'static_pages#about'
   get  '/home', to: 'static_pages#home'
+  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   get  '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
